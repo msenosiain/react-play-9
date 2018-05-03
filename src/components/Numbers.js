94ae88c7678510cc9dab2f56ceabfb954f9c735c
Miguel Senosiain <msenosiain@gmail.com>
2018-05-03 15:18:04 -0300

    Add config files after eject

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import _ from "lodash";
-import "./Numbers.css";
+import _ from 'lodash';
+import './Numbers.css';
 
 const Numbers = (props) => {
     const numberClassName = (number) => {
