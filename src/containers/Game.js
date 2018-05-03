b19fd33f7e25f2aa35a2a9cddff26aa43416dd7e
Miguel Senosiain <miguel.senosiain@globant.com>
2018-05-03 11:49:04 -0300

    Add Button component

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -30,41 +30,7 @@ var possibleCombinationSum = function (arr, n) {
 };
 
 
-const Button = (props) => {
-    let button;
-    switch (props.answerIsCorrect) {
-        case true:
-            button =
-                <button className="btn btn-success" onClick={props.acceptAnswer}>
-                    <i className="fa fa-check"></i>
-                </button>;
-            break;
-        case false:
-            button =
-                <button className="btn btn-danger">
-                    <i className="fa fa-times"></i>
-                </button>;
-            break;
-        default:
-            button =
-                <button className="btn"
-                        onClick={props.checkAnswer}
-                        disabled={props.selectedNumbers.length === 0}>
-                    =
-                </button>;
-            break;
-    }
-    return (
-        <div className="col-1 text-center">
-            {button}
-            <br/><br/>
-            <button className="btn btn-warning btn-sm" onClick={props.redraw}
-                    disabled={props.redraws === 0}>
-                <i className="fa fa-redo"></i> {props.redraws}
-            </button>
-        </div>
-    );
-};
+
 
 const Answer = (props) => {
     return (
